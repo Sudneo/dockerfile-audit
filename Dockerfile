0f8aa4e88d6d98862fd6ba9928b98d7c2ee1f05a
sudneo <mucci.daniele@gmail.com>
2020-03-07 16:23:20 +0200

    Successfully parses FROM commands and comments

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+ FROM localhost:5000/myimage@sha256:47bfdb88c3ae13e488167607973b7688f69d9e8c142c2045af343ec199649c09
+FROM debian:buster-slim AS build
+FROM https://test-123.com/image AS test
+FROM http://test-123.com:4682/image AS test
+
+#
+# LABEL maintainer="NGINX Docker Maintainers <docker-maint@nginx.com>"
+#
+# ENV NGINX_VERSION   1.17.9
+# ENV NJS_VERSION     0.3.9
+# ENV PKG_RELEASE     1~buster
+#
+# RUN set -x \
+#     && addgroup --system --gid 101 nginx \
+#     && adduser --system --disabled-login --ingroup nginx --no-create-home --home /nonexistent --gecos "nginx user" --shell /bin/false --uid 101 nginx
--- parser.py
+++ parser.py
@@ -4,22 +4,143 @@ from parsimonious.nodes import NodeVisitor
 
 grammar = Grammar(
     """
-    line                  = command ws parameters newline
-    parameters            = word_symbols (ws+ word_symbols)*
-    command               = ws? capital_word
-    capital_word          = ~"[A-Z]+"
+    dockerfile            = (comment / from_command / ws)*
+    from_command          = from spaces (platform)? registry? image_name (image_tag / digest)? (local_name)? ws
+    
+    from                  = spaces* "FROM"
+    platform              = "--platform=" word_symbols spaces
+    registry              = host (":" port)? "/"
+    host                  = (protocol)? ~"[a-zA-Z0-9.-]+"
+    protocol              = ("https://" / "http://")
+    port                  = ~"[0-9]{1,5}"
+    image_name            = ~"[a-zA-Z0-9][a-zA-Z0-9_.-]+"
+    image_tag             = ":" ~"[\w][\w.-]{0,127}"
+    digest                = "@" algorithm ":" hash
+    algorithm             = ~"[a-zA-Z0-9]+"
+    hash                  = ~"[a-z0-9]{32,}"
+    local_name            = spaces "AS" spaces word_symbols
+    
+    comment               = comment_start sentence* ws
+    sentence              = spaces* word_symbols (spaces word_symbols)*
+    comment_start         = spaces* hashtag spaces*
+    hashtag               = "#"
+    spaces                = space+
+    space                 = " "
     word_symbols          = ~"[\S]+"
     ws                    = ~"\s+"
-    newline               = ~"\s*"
     """
 )
 
 
 class IniVisitor(NodeVisitor):
-    def visit_line(self, node, visited_children):
+    def visit_dockerfile(self, node, visited_children):
         """ Returns the overall output. """
-        command, _, parameters, _ = visited_children
-        return {'command': command, 'parameters': parameters}
+        result = {'comments': [],
+                  'commands': {
+                      'from_commands': []
+                    }
+                  }
+        for line in visited_children:
+            if line[0]['type'] == "comment":
+                content = line[0]['content']
+                if content is not None and content != "":
+                    result['comments'].append(content)
+            if line[0]['type'] == "command":
+                if line[0]['command_type'] == "from":
+                    result['commands']['from_commands'].append(line[0]['content'])
+        return result
+
+    def visit_comment(self, node, visited_children):
+        _, comment, _ = visited_children
+        comment_sentence = ""
+        for item in comment:
+            comment_sentence += f"{item}"
+        return {"type": "comment", "content": comment_sentence}
+
+    def visit_from_command(self, node, visited_children):
+        _, _, platform, registry, image_name, tag_or_digest, local_name, _ = visited_children
+        try:
+            local_build_name = local_name[0]
+        except:
+            local_build_name = None
+        try:
+            registry_url = registry[0]
+        except:
+            registry_url = "Docker Hub"
+        try:
+            tag = tag_or_digest[0][0]
+        except:
+            tag = "latest"
+        result = {"type": "command",
+                  "command_type": "from",
+                  "content":
+                      {
+                       "image": image_name.text,
+                       "registry": registry_url,
+                       "tag": tag,
+                       "local_name": local_build_name,
+                       },
+                  "raw": node.text
+                  }
+        return result
+
+    def visit_from(self, node, visited_children):
+        return "FROM"
+
+    def visit_platform(self, node, visited_children):
+        _, platform, _ = visited_children
+        return platform.text
+
+    def visit_registry(self, node, visited_children):
+        host, port, _ = visited_children
+        try:
+            port_number = port[0][1]
+            registry_url = f"{host}:{port_number}"
+        except:
+            registry_url = host
+        return registry_url
+
+    def visit_host(self, node, visited_children):
+        protocol, host = visited_children
+        try:
+            proto = protocol[0]
+            return f"{proto}{host.text}"
+        except:
+            return host.text
+
+    def visit_protocol(self, node, visited_children):
+        return node.text
+
+    def visit_port(self, node, visited_children):
+        return node.text
+
+    def visit_image_name(self, node, visited_children):
+        return node
+
+    def visit_image_tag(self, node, visited_children):
+        _, tag = visited_children
+        return tag.text
+
+    def visit_digest(self, node, visited_children):
+        _, algorithm, _, digest = visited_children
+        return f"@{algorithm.text}:{digest.text}"
+
+    def visit_local_name(self, node, visited_children):
+        _, _, _, name = visited_children
+        return name.text
+
+    def visit_sentence(self, node, visited_children):
+        _, word, words = visited_children
+        sentence = ""
+        if word is not None:
+            sentence += word.text
+        if words[0][1] is not None:
+            sentence += f" {words[0][1].text}"
+        return sentence
+
+    def visit_comment_start(self, node, visited_children):
+        _, hashtag, _ = visited_children
+        return hashtag.text
 
     def visit_parameters(self, node, visited_children):
         """ Makes a dict of the section (as key) and the key/value pairs. """
@@ -50,17 +171,11 @@ class IniVisitor(NodeVisitor):
 
 
 with open('Dockerfile') as f:
-    data = f.readlines()
-    print(data)
-    for line in data:
-        if line != "\n":
-            tree = grammar.parse(line)
-            iv = IniVisitor()
-            print(iv.visit(tree))
+    data = f.read()
+    #print(data)
+    tree = grammar.parse(data)
+    #print(tree)
+    iv = IniVisitor()
+    print(iv.visit(tree))
 
-# tree = grammar.parse(data)
-#
-# iv = IniVisitor()
-# output = iv.visit(tree)
-# print(output)
 
