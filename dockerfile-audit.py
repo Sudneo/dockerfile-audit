fc422b9e82c40e313dc5822ffb989463f0c6dd8f
sudneo <mucci.daniele@gmail.com>
2020-03-19 20:11:28 +0200

    Adds more command line options for report generation.

--- dockerfile-audit.py
+++ dockerfile-audit.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import argparse
 import logging
 import jinja2
@@ -13,44 +14,17 @@ def get_args():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-d", "--dockerfile", type=str, help="The Dockerfile to audit.")
     group.add_argument("-b", "--batch", type=str, help="A directory in which all files will be audited.")
+    parser.add_argument("-r", "--report", action='store_true', help="Generate a PDF report about the findings")
+    parser.add_argument("-n", "--report-name", default="report.pdf", help="The name of the PDF report.")
+    parser.add_argument("-t", "--report-template", default="templates/report-template.tex",
+                        help="The template for the report to use")
     args = parser.parse_args()
     return args
 
 
-def generate_report(policy, policy_results):
-    total_tests = len(policy_results)
-    successful_tests = len([test for test in policy_results if test['audit-outcome'] == 'pass'])
-    failed_tests = total_tests - successful_tests
-    success_percentage = round((successful_tests * 100) / total_tests, 2)
-    failed_percentage = round((failed_tests * 100) / total_tests, 2)
+def generate_report(policy, policy_results, template, outfile):
     failure_stats = get_rules_violation_stats(policy_results, policy)
-    compliance_level = "N/A"
-    compliance_color = "red"
-    if success_percentage < 10:
-        compliance_level = "Poor"
-        compliance_color = "red!50"
-    elif success_percentage < 25:
-        compliance_level = "Low"
-        compliance_color = "red!30"
-    elif success_percentage < 50:
-        compliance_level = "Medium"
-        compliance_color = "orange!50"
-    elif success_percentage < 80:
-        compliance_level = "Fair"
-        compliance_color = "green!20"
-    elif 80 < success_percentage < 100:
-        compliance_level = "Good"
-        compliance_color = "green!35"
-    elif success_percentage == 100:
-        compliance_level = "Perfect"
-        compliance_color = "green!50"
-    summary_stats = {'total_tests': total_tests,
-                     'success_tests': successful_tests,
-                     'failed_tests': failed_tests,
-                     'success_percentage': str(success_percentage),
-                     'failed_percentage': str(failed_percentage),
-                     'compliance_level': compliance_level,
-                     'compliance_color': compliance_color}
+    summary_stats = get_summary_stats(policy_results)
     enabled_policy_rules = {'policy_rules_enabled': policy.get_policy_rules_enabled()}
     for enabled_rule in enabled_policy_rules['policy_rules_enabled']:
         enabled_rule['type'] = latex_escape(enabled_rule['type'])
@@ -83,7 +57,7 @@ def generate_report(policy, policy_results):
         autoescape=False,
         loader=jinja2.FileSystemLoader(os.path.abspath('.'))
     )
-    template = latex_jinja_env.get_template('templates/report-template.tex')
+    template = latex_jinja_env.get_template(template)
     rendered_template = template.render(summary_stats=summary_stats,
                                         failure_stats=failure_stats,
                                         enabled_policy_rules=enabled_policy_rules,
@@ -94,10 +68,64 @@ def generate_report(policy, policy_results):
     out_file = f"{build_dir}/template"
     with open(out_file, "w") as f:
         f.write(rendered_template)
-    os.system(f"cp -r templates/images {build_dir}")
-    os.system(f"cd {build_dir} && pdflatex -output-directory {os.path.realpath(build_dir)} "
-              f"{os.path.realpath(out_file)}")
-    copyfile(f"{out_file}.pdf", "report.pdf")
+    try:
+        subprocess.run(["cp", "-r", "templates/images", build_dir], check=True)
+    except subprocess.CalledProcessError as error:
+        logger.error("The copy of templates/images directory into the build directory failed."
+                     " Images might not be rendered")
+        logger.error(error)
+    try:
+        subprocess.run(f"cd {build_dir} && pdflatex -output-directory {os.path.realpath(build_dir)}"
+                       f" {os.path.realpath(out_file)}",
+                       check=True, shell=True)
+    except subprocess.CalledProcessError as error:
+        logger.error("The first Pdflatex iteration failed.")
+        logger.error(error)
+    try:
+        # build a second time for TOC
+        subprocess.run(f"cd {build_dir} && pdflatex -output-directory {os.path.realpath(build_dir)}"
+                       f" {os.path.realpath(out_file)}",
+                       check=True, shell=True)
+    except subprocess.CalledProcessError as error:
+        logger.error("The second Pdflatex iteration failed.")
+        logger.error(error)
+    copyfile(f"{out_file}.pdf", outfile)
+
+
+def get_summary_stats(policy_results):
+    total_tests = len(policy_results)
+    successful_tests = len([test for test in policy_results if test['audit-outcome'] == 'pass'])
+    failed_tests = total_tests - successful_tests
+    success_percentage = round((successful_tests * 100) / total_tests, 2)
+    failed_percentage = round((failed_tests * 100) / total_tests, 2)
+    compliance_level = "N/A"
+    compliance_color = "red"
+    if success_percentage < 10:
+        compliance_level = "Poor"
+        compliance_color = "red!50"
+    elif success_percentage < 25:
+        compliance_level = "Low"
+        compliance_color = "red!30"
+    elif success_percentage < 50:
+        compliance_level = "Medium"
+        compliance_color = "orange!50"
+    elif success_percentage < 80:
+        compliance_level = "Fair"
+        compliance_color = "green!20"
+    elif 80 < success_percentage < 100:
+        compliance_level = "Good"
+        compliance_color = "green!35"
+    elif success_percentage == 100:
+        compliance_level = "Perfect"
+        compliance_color = "green!50"
+    summary_stats = {'total_tests': total_tests,
+                     'success_tests': successful_tests,
+                     'failed_tests': failed_tests,
+                     'success_percentage': str(success_percentage),
+                     'failed_percentage': str(failed_percentage),
+                     'compliance_level': compliance_level,
+                     'compliance_color': compliance_color}
+    return summary_stats
 
 
 def get_rules_violation_stats(policy_results, policy):
@@ -157,7 +185,11 @@ def main():
                 continue
             policy_result = policy.evaluate_dockerfile(d)
             policy_results.append(policy_result)
-    generate_report(policy, policy_results)
+    if arguments.report:
+        logger.debug("Preparing to generate PDF report.")
+        template = arguments.report_template
+        outfile = arguments.report_name
+        generate_report(policy, policy_results, template, outfile)
 
 
 if __name__ == '__main__':
